2d5a26abdcf30bdfc9a2355769ca1e845587d0ee
remico <remicollab@gmail.com>
2021-08-24 22:24:22 +0300

    new buttons' icons

--- scripts/build.sh
+++ scripts/build.sh
@@ -18,25 +18,29 @@ if [[ $1 == "release" ]]; then
     BUILD_DIR=bin
     BUILD_TYPE="-r"
     EXT="prg"
+    D="-d $DEVICE"
 elif [[ $1 == "publish" ]]; then
     BUILD_DIR=bin
     BUILD_TYPE="-e -r"
     EXT="iq"
+    D=""  # all devices
 elif [[ $1 == "debug" ]]; then
     BUILD_DIR=bin
     BUILD_TYPE=""
     EXT="prg"
+    D="-d $DEVICE"
 else  # simulator
     BUILD_DIR=bin
     [[ $1 == "simulator_release" ]] && BUILD_TYPE="-r"
     EXT="prg"
     SIMULATOR=1
+    D="-d $DEVICE"
 fi
 
 
 TARGET=${workspaceFolder}/${BUILD_DIR}/${PRJ_NAME}.${EXT}
 
-java -Dfile.encoding=UTF-8 -Dapple.awt.UIElement=true -jar ${SDK}/monkeybrains.jar -o ${TARGET} -w -y ${DKEY} -d ${DEVICE} ${BUILD_TYPE} -f ${workspaceFolder}/monkey.jungle
+java -Dfile.encoding=UTF-8 -Dapple.awt.UIElement=true -jar ${SDK}/monkeybrains.jar -o ${TARGET} -w -y ${DKEY} ${D} ${BUILD_TYPE} -f ${workspaceFolder}/monkey.jungle
 
 if [[ $? -eq 0 ]] && [[ ${SIMULATOR} ]]; then
     adb connect 192.168.0.101
